1514400453dc0be80298ba3c0eefa4863479a88b
Prashu328 <91869879+AkshuPrashu@users.noreply.github.com>
2021-10-07 20:45:07 +0530

    Create Bancodes.py

---
+++ Sibyl_System/plugins/GbanWatch/Sibyl_System/plugins/Bancodes.py
@@ -0,0 +1,26 @@
+
+help_plus = """ Here is Help for **GbanWatch Bancodes** :-
+**❂** `{SRx00}` **SCAMMER.**
+**❂** `{SRx01}` **SPAM**
+**❂** `{SRx02}` **TOXIC ABUSE SPAM.**
+**❂** `{SRx04}` **NSFW SPAMMER.**
+**❂** `{SRx05}` **LICENSE VIOLATIONS/COPYRIGHT.**
+**❂** `{SRx06}` **IMPERSONATION.**
+**❂** `{SRx07}` **MD/BTC SCAM.**
+**❂** `{SRx08}` **ADDING SPAMBOTS.**
+**❂** `{SRx10}` **ILLEGAL.**
+**❂** `{SRx11}` **PHISHING.**
+**❂** `{SRx12}` **FRAUD PROMOTION  [ANY KIND].**
+**❂** `{SRx13}` **CYBER THREATENING / CYBER BULLY.**
+**❂** `{SRx14}` **CRIMES AGAINST GIRLS BLACKMAIL SEXUAL ABUSE.**
+**❂** `{SRx15}` **LEAKING PRIVACY POLICY PIC,NUMBER.**
+**❂** `{SRx16}` **GENERIC ABUSE.**
+**❂** `{SRx17}` **BAN EVASION.** 
+**❂** `{SRx18}` **SPAMBOT [NOT SAFE].**
+**❂** `{SRx19}` **RAID/SPAM INFLAMMER.**
+**❂** `{SRx20}` **SPAM/RAID PARTICIPANTS.**
+**❂** `{SRx21}` **KRIMINALANT.**
+
+**THE BANCODE IS DERIVED FROM @SPAMREPORTER**
+"""
+__plugin_name__ = "bancodes
